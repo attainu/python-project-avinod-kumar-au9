2f6f8767706543320c6063e4b5fa28b0c29f683d
A vinod kumar <asundi.vinodkumar888@gmail.com>
2020-09-16 17:33:22 +0530

    added code,problem statement

---
+++ parking_lot code.py
@@ -0,0 +1,201 @@
+
+import time
+
+import datetime
+current_date_time= datetime.datetime.now()
+
+class vehicle_Parking:
+    def __init__(self):
+        pass
+
+    def creating_parking_slots(self,create_parking_lot):
+        
+
+        for i in range(1,(create_parking_lot)+1):
+            if i not in dicti:
+                dicti['slot_'+str(i)]="a"
+
+        # print(dicti)
+        print("Created a parking lot with",create_parking_lot,"slots")
+
+    
+    def parking_vehicle_at_slots(self,vehicle_for_parking):
+        checking_parking_lot_is_full=0
+        
+        for i in range(1,(create_parking_lot)+1):
+            checking_parking_lot_is_full+=1
+            
+            checking_given_car_in_parking_lot= 0
+            for keys,values in dicti.items():
+                if vehicle_for_parking[0] in values:
+                    checking_given_car_in_parking_lot+=1
+                    break
+            
+
+            if checking_given_car_in_parking_lot == 1:
+                    print("Car already in parking slot, recheck your entry")
+                    break
+            
+            if checking_given_car_in_parking_lot != 1:
+                if dicti["slot_"+str(i)]=="a":
+                    
+                    dicti["slot_"+str(i)]=vehicle_for_parking
+                    print("Allocated slot number:", i,"on",current_date_time.strftime('%d-%m-%Y'),current_date_time.strftime("%H:%M"))
+                    # print(dicti)
+                    break
+            
+            if checking_given_car_in_parking_lot!= 1:    
+                if checking_parking_lot_is_full==create_parking_lot:
+            
+                        print("Sorry, parking lot is full")
+
+        
+    def Leaving_vehicle_from_parking_lot(self,Leave,create_parking_lot):
+
+        if int(Leave) > create_parking_lot:
+            print("Slot number",Leave,"is not found")
+        if int(Leave) <= create_parking_lot:
+            if dicti['slot_'+str(Leave)]=="a":
+                print("Slot number",Leave,"is already free")
+        if int(Leave) <= create_parking_lot:
+            if dicti['slot_'+str(Leave)]!="a":
+                dicti['slot_'+str(Leave)]="a"
+                print("Slot number",Leave,"is free")
+
+
+    def getParkingStatus(self):
+        print("This data as on:",current_date_time.strftime('%d-%m-%Y'),current_date_time.strftime("%H:%M"))
+        print("Slot No. || Registration No || Colour")
+        for keys,values in dicti.items():
+            if values != "a":
+                print(keys[5:],values[0],values[1])
+            
+
+    def get_registration_numbers_for_cars_with_colour(self,colour):
+        str=""
+        for keys,values in dicti.items():
+            if colour in values:
+                str+=values[0]+", "
+        
+        print(str[:-2])
+        
+
+    def get_slot_numbers_for_cars_with_colour(self,colour):
+        str=""
+        for keys,values in dicti.items():
+            if colour in values:
+                str+=keys[5:]+", "
+        print(str[:-2])
+        
+
+
+    def get_slot_number_for_registration_number(self,registration_number):
+        for keys,values in dicti.items():
+            if registration_number in values:
+                return (keys[5:])
+        else:
+            return ("Not found")
+    
+
+    def show_line(self,user_input):
+        user_input=user_input.split()
+        
+        
+        if "create_parking_lot" in user_input:
+            
+            global create_parking_lot
+            create_parking_lot=int(user_input[-1])
+            parking.creating_parking_slots(create_parking_lot)
+        
+        
+        if "park" in user_input:
+            
+            vehicle_for_parking= user_input[1]+" "+user_input[2]
+            vehicle_for_parking=vehicle_for_parking.split()
+            parking.parking_vehicle_at_slots(vehicle_for_parking)
+
+
+        if "leave" in user_input:
+            
+            Leave = user_input[1]
+            parking.Leaving_vehicle_from_parking_lot(Leave,create_parking_lot)
+
+        
+        if "status" in user_input:
+            parking.getParkingStatus()
+
+        
+        if "registration_numbers_for_cars_with_colour" in user_input:
+            colour= user_input[-1]
+            parking.get_registration_numbers_for_cars_with_colour(colour)
+
+        
+        if "slot_numbers_for_cars_with_colour" in user_input:
+            colour=user_input[-1]
+            parking.get_slot_numbers_for_cars_with_colour(colour)
+
+
+        if "slot_number_for_registration_number" in user_input:
+            registration_number = user_input[-1]
+            parking.get_slot_number_for_registration_number(registration_number)
+            print(parking.get_slot_number_for_registration_number(registration_number))
+
+
+def main():
+    
+    global dicti
+    dicti=dict()
+    print("\n")
+    print("---- Welcome to the automated parking lot system ----")
+    
+    print("\n")
+    time.sleep(2)
+    print("System Initializing...")
+    time.sleep(1)
+    print("\n")
+    
+    while True:
+        user_input=input("Choose your choice - interactive or file ?: ").lower()
+        
+        if "interactive" in user_input:
+            break
+        if "file" in user_input:
+            break
+        if "exit" in user_input:
+            print("Thank you for mingling with Me!")
+            break
+        if "interactive" not in user_input or "file" not in user_input:
+            print("Human error!, Choose a right choice")
+    
+    print("\n")
+    if "interactive" in user_input:
+        
+        while True:
+            print("----------------------------")
+            user_input=input("What can i do for you ?: ")
+            if user_input=="exit":
+                print("\n")
+                print("Thank you for mingling with Me!")
+                print("\n")
+                break
+            parking.show_line(user_input)
+            
+
+    if "file" in user_input:
+   
+        with open("D:\Vinod\practise/1.txt","r") as f:
+            for line in f:
+                user_input=line
+                parking.show_line(user_input)
+            print("\n")
+            print("Thank you for mingling with Me!")
+            print("\n")
+
+
+
+if __name__ == "__main__":
+    
+    
+    parking= vehicle_Parking()
+    main()
+
